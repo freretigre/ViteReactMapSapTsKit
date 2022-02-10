740c0f8d455a0e3c2bb2dfb4f8fbf4cdae61537f
jecky <jecky@sing.com>
2022-02-10 09:23:41 +0700

    add carousel

--- src/pages/analysis/index.tsx
+++ src/pages/analysis/index.tsx
@@ -1,12 +1,14 @@
 import { memo } from 'react';
 import SelectLang from '../../../src/components/SelectLang';
 import Counter from "../../components/Counter";
+import Carousels from "../../components/Carousels";
 import styles from './index.module.scss';
 
 const Index = memo(() => {
     return (
         <div className={styles.analysis}>
             <h1>analysis-分析页</h1>
+            <Carousels />
             <SelectLang />
             <div>
                 <Counter />
