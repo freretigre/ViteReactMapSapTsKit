fd3ee45ce71c052e067a88f5c61580b91cb2673a
jecky <jecky@sing.com>
2022-02-12 00:30:58 +0700

    add redux saga

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { store } from "./models/store";
+import { rootStore } from "./models/store";
 import './global.scss'
 import Routers from "./routers";
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
+      <Provider store={rootStore}>
           <Routers />
       </Provider>
   </React.StrictMode>,
--- src/models/store/index.ts
+++ src/models/store/index.ts
@@ -1,13 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware, {SagaMiddleware} from 'redux-saga';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { rootSlices } from "../slices";
+import rootSaga from "../sagas";
 
-export const store = configureStore({
-    reducer: {
-        counter: rootSlices.reducer,
-    },
-    middleware: [thunk, logger] as const,
-});
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export const configureRootStore: any = () => {
+    const sagaMiddleware: SagaMiddleware<object> = createSagaMiddleware();
+    const middlewares = [sagaMiddleware, thunk, logger];
+    const middleware = [
+        ...middlewares,
+    ];
+    const store = configureStore({
+        reducer: {
+            counter: rootSlices.reducer,
+        },
+        middleware: middleware,
+        devTools: true,
+    });
+
+    sagaMiddleware.run(rootSaga);
+    return store;
+};
+
+export const rootStore = configureRootStore();
+
+export type RootState = ReturnType<typeof rootStore.getState>;
+export type AppDispatch = typeof rootStore.dispatch;
